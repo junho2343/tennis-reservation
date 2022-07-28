b63139137a2a61d7d59ac70a332bc909cfa61add
junho2343 <wnsak2343@naver.com>
2022-07-28 14:01:25 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# tennis-reservation
-광명도시 테니스장 예약
+
+### 광명도시공사 테니스장 예약
+https://reserve.gmuc.co.kr/user/tennis/tennisMain.do?menuFlag=T
+--- 
+
+- npm install -g typescript
+- npm i
+- 예약 할 계정 로그인 및 쿠키 정보 복사
+  - https://reserve.gmuc.co.kr/user/tennis/tennisMain.do?menuFlag=T
+- 예약할 날짜, 코트, 인원 정보 및 쿠키 입력
+- npm run start (예약 소스 실행)
+- 결제는 사이트 접속 후 직접 해야함
+
---
+++ app.ts
@@ -0,0 +1,294 @@
+const qs = require("qs");
+const { default: axios } = require("axios");
+
+const reservationMemberData: IReservationMemberData[] = [
+  ////////////////////////////////////////////////////
+  // 인철이형 계정
+  ////////////////////////////////////////////////////
+  {
+    nickname: "인철이형 계정",
+    cookie: "JSESSIONID=",
+    reservationData: [
+      // 주말반
+      {
+        reservDate: "20220807",
+        timeCode: "TM046",
+        courtCode: "TC001",
+        adultCnt: 3,
+      },
+      {
+        reservDate: "20220807",
+        timeCode: "TM047",
+        courtCode: "TC001",
+        adultCnt: 3,
+      },
+
+      {
+        reservDate: "20220814",
+        timeCode: "TM046",
+        courtCode: "TC001",
+        adultCnt: 3,
+      },
+      {
+        reservDate: "20220814",
+        timeCode: "TM047",
+        courtCode: "TC001",
+        adultCnt: 3,
+      },
+
+      // 평일반
+      {
+        reservDate: "20220804",
+        timeCode: "TM052",
+        courtCode: "TC001",
+        adultCnt: 3,
+      },
+      {
+        reservDate: "20220809",
+        timeCode: "TM052",
+        courtCode: "TC001",
+        adultCnt: 3,
+      },
+    ],
+  },
+
+  ////////////////////////////////////////////////////
+  // 다른 계정
+  ////////////////////////////////////////////////////
+  {
+    nickname: "형수님 계정",
+    cookie: "JSESSIONID=",
+    reservationData: [
+      // 주말반
+      {
+        reservDate: "20220807",
+        timeCode: "TM046",
+        courtCode: "TC002",
+        adultCnt: 2,
+      },
+      {
+        reservDate: "20220807",
+        timeCode: "TM047",
+        courtCode: "TC002",
+        adultCnt: 2,
+      },
+      {
+        reservDate: "20220814",
+        timeCode: "TM046",
+        courtCode: "TC002",
+        adultCnt: 2,
+      },
+      {
+        reservDate: "20220814",
+        timeCode: "TM047",
+        courtCode: "TC002",
+        adultCnt: 2,
+      },
+    ],
+  },
+];
+
+start();
+
+async function start() {
+  for (const reservationMemberDataOne of reservationMemberData) {
+    console.log(`${reservationMemberDataOne.nickname} 계정 예약 시작`);
+
+    for (const reservationDataOne of reservationMemberDataOne.reservationData) {
+      try {
+        // 날짜 선택
+        await axiosProcessing({
+          url: "/user/tennis/tennisInsideReservDayCheck.do",
+          data: {
+            reservDate: reservationDataOne.reservDate,
+          },
+          cookie: reservationMemberDataOne.cookie,
+        });
+
+        // 시간 선택
+        await axiosProcessing({
+          url: "/user/tennis/tennisReservNext0Check.do",
+          data: {
+            timeCode: reservationDataOne.timeCode,
+            fromTime: FromTime[reservationDataOne.timeCode],
+            toTime: ToTime[reservationDataOne.timeCode],
+            menuId: "Resv",
+          },
+          cookie: reservationMemberDataOne.cookie,
+        });
+
+        // 코트 선택
+        await axiosProcessing({
+          url: "/user/tennis/tennisReservNext1Check.do",
+          data: {
+            courtCode: reservationDataOne.courtCode,
+            courtNo: CourtNo[reservationDataOne.courtCode],
+            menuId: "Resv",
+          },
+          cookie: reservationMemberDataOne.cookie,
+        });
+
+        // 이용 구분
+        await axiosProcessing({
+          url: "/user/tennis/tennisReservNext2Check.do",
+          data: {
+            useTypeCd: "002",
+            useTypeNm: "연습이용",
+            menuId: "Resv",
+          },
+          cookie: reservationMemberDataOne.cookie,
+        });
+
+        // 인원 선택
+        await axiosProcessing({
+          url: "/user/tennis/tennisReservNext3Check.do",
+          data: {
+            adultCnt: reservationDataOne.adultCnt,
+            youthCnt: "0",
+            oldManCnt: "0",
+            gCardCnt: "0",
+            mChildCnt: "0",
+            useLightYn: "N",
+            menuId: "Resv",
+          },
+          cookie: reservationMemberDataOne.cookie,
+        });
+
+        // 최종 예약
+        await axiosProcessing({
+          url: "/user/tennis/tennisReservNext4Check.do",
+          data: {
+            deal_type: "CARD",
+            menuId: "Resv",
+          },
+          cookie: reservationMemberDataOne.cookie,
+        });
+
+        // console.log();
+      } catch (error) {
+        console.log(
+          `${reservationDataOne.reservDate} - ${
+            FromTime[reservationDataOne.timeCode]
+          } - ${CourtNo[reservationDataOne.courtCode]}코트 예약 실패`
+        );
+      }
+    }
+  }
+}
+
+async function axiosProcessing({ url, data, cookie }) {
+  const result = await axios.post(
+    "https://reserve.gmuc.co.kr" + url,
+    qs.stringify(data),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Cookie: cookie,
+      },
+    }
+  );
+
+  // 날짜 선택 후 예약 가능 시간 확인
+  // if(url === "/user/tennis/tennisReservDayCheck.do"){
+  //   result.data.includes(`id="${data.}'`)
+  // }
+
+  // 최종 예약은 결과 표시하기
+  if (url === "/user/tennis/tennisReservNext4Check.do") {
+    console.log(result.data);
+  }
+}
+
+interface IReservationMemberData {
+  nickname: string;
+  cookie: string;
+  reservationData: IReservationData[];
+}
+interface IReservationData {
+  /**
+   * 예약날짜
+   */
+  reservDate: string;
+
+  /**
+   * 예약 시간
+   */
+  timeCode:
+    | "TM045" // 06:00 ~ 08:00
+    | "TM046" // 08:00 ~ 10:00
+    | "TM047" // 10:00 ~ 12:00
+    | "TM048" // 12:00 ~ 14:00
+    | "TM049" // 14:00 ~ 16:00
+    | "TM050" // 16:00 ~ 18:00
+    | "TM051" // 18:00 ~ 20:00
+    | "TM052"; // 20:00 ~ 22:00
+  // fromTime: "18:00",
+  //   toTime: "20:00",
+
+  /**
+   * 예약 코트 코드
+   */
+  courtCode:
+    | "TC001"
+    | "TC002"
+    | "TC003"
+    | "TC004"
+    | "TC005"
+    | "TC006"
+    | "TC007"
+    | "TC008"
+    | "TC009"
+    | "TC010"
+    | "TC011"
+    | "TC012"
+    | "TC013";
+  // courtNo: "2",
+
+  // /**
+  //  * 이용 구분
+  //  */
+  // useTypeCd: "002",
+  // useTypeNm: "연습이용",
+
+  /**
+   * 성인 인원
+   */
+  adultCnt: number;
+}
+
+enum FromTime {
+  "TM045" = "06:00",
+  "TM046" = "08:00",
+  "TM047" = "10:00",
+  "TM048" = "12:00",
+  "TM049" = "14:00",
+  "TM050" = "16:00",
+  "TM051" = "18:00",
+  "TM052" = "20:00",
+}
+enum ToTime {
+  "TM045" = "08:00",
+  "TM046" = "10:00",
+  "TM047" = "12:00",
+  "TM048" = "14:00",
+  "TM049" = "16:00",
+  "TM050" = "18:00",
+  "TM051" = "20:00",
+  "TM052" = "22:00",
+}
+
+enum CourtNo {
+  "TC001" = "1",
+  "TC002" = "2",
+  "TC003" = "3",
+  "TC004" = "4",
+  "TC005" = "5",
+  "TC006" = "6",
+  "TC007" = "7",
+  "TC008" = "8",
+  "TC009" = "9",
+  "TC010" = "10",
+  "TC011" = "11",
+  "TC012" = "12",
+  "TC013" = "13",
+}
